2d05e9d19e4a0efd96dd19350337d5a5a66cf197
Leo Mak <leo.mak@ntlworld.com>
2012-12-01 00:39:06 +0000

    updated square

--- Reversi/src/Game/Square.java
+++ Reversi/src/Game/Square.java
@@ -1,4 +1,4 @@
-package Game;
+
 
 public enum Square {
 	EMPTY {
@@ -6,6 +6,11 @@ public enum Square {
 	    public String toString() {
 	        return " ";
 	    }
+		
+		@Override
+		public Square opposite(){
+			return EMPTY;
+		}
 	},
 	 
 	BLACK {
@@ -13,11 +18,27 @@ public enum Square {
 	    public String toString() {
 	        return "X";
 	    }
+		
+		@Override
+		public Square opposite(){
+			return WHITE;
+		}
 	},
 	WHITE {
 		@Override
 		public String toString(){
 			return "O";
 		}
+		
+		@Override
+		public Square opposite(){
+			return BLACK;
+		}
+	};
+
+	public Square opposite() {
+		return null;
 	}
+
+
 }
