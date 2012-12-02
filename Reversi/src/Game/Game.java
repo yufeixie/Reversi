a57b484bd438141cf8db4cbdb16859ad4f7d1e7b
Miten Mistry <mistry783@hotmail.co.uk>
2012-12-02 01:36:19 +0000

    working

--- Reversi/src/Game/Game.java
+++ Reversi/src/Game/Game.java
@@ -1,23 +1,67 @@
 package Game;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Game {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String name;
+		int currentPlayer = 0;
+		ArrayList<Direction>validDirections;
+		Square currentPiece;
+		int x, y, scoreChange;
+		String nextCoordinate;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Reversi");
+		Player[] players = new Player[2];
 		Board board = checkCustomBoard(input);
-		//System.out.println("Player 1, enter your name:");
-		//name = input.nextLine();
-		//System.out.println("Player 2, enter your name:");
-		//name = input.nextLine();
+		players[0] = createNewPlayer(1, new Piece(Square.WHITE), input);
+		players[1] = createNewPlayer(2, new Piece(Square.BLACK), input);
+		System.out.println("Press any key.");
+		input.nextLine();
 		board.display();
 		
-		System.out.println(board.isValidMove(3, 4, Square.BLACK));
-
+		
+		while(!board.gameOver()){
+			
+			currentPiece = players[currentPlayer].getPiece().getPiece();
+			
+			if (!board.validMoveExists(currentPiece)){
+				System.out.println("Player " + (currentPlayer + 1) + " cannot move.");
+				currentPlayer = ++currentPlayer%2;
+				currentPiece = players[currentPlayer].getPiece().getPiece();
+			}
+			
+			do {				
+				System.out.println("Player " + (currentPlayer + 1) + ": Please enter your move in the format: letter number");
+								
+				nextCoordinate = input.next();
+				x = 1 + (nextCoordinate.toUpperCase().charAt(0) - 'A');
+				nextCoordinate = input.next();
+				y = Integer.parseInt(nextCoordinate);
+				
+				validDirections = board.checkMoves(x, y, currentPiece);
+			} while((validDirections == null));
+	
+			scoreChange = board.makeMove(x, y, currentPiece, validDirections);
+			players[currentPlayer].setScore(scoreChange);
+			currentPlayer = ++currentPlayer%2;
+			players[currentPlayer].setScore(-(--scoreChange));
+			board.display();			
+		}
+		System.out.println("Game Over!");
+		int player1Score = players[0].getScore();
+		int player2Score = players[1].getScore();
+		
+		if (player1Score > player2Score){
+			System.out.println("Player 1 wins with a score of " + player1Score);
+		} else if (player1Score < player2Score){
+			System.out.println("Player 2 wins with a score of " + player2Score);
+		} else {
+			System.out.println("It's a draw!");
+			System.out.println("The final score for both players is " + player1Score);
+		}
 		input.close();
 	}
 
@@ -48,7 +92,17 @@ public class Game {
 		}
 
 		return new Board();
+	}
 
+	private static Player createNewPlayer(int playerNumber, Piece piece,
+			Scanner input) {
+		String name;
+		System.out.println("Player " + playerNumber
+				+ " please enter your name: ");
+		name = input.nextLine();
+		System.out.println("Hello " + name + ", you will be player "
+				+ playerNumber + " and your piece is : " + piece.getPiece());
+		return new Player(name, 2, piece);
 	}
 
 
